e85d13325a9dcc985ae0d4994752e49b9b183440
Maksim Ivanov <m0597@berkeley.edu>
2022-12-16 17:38:21 -0500

    extract flag

--- server/binder/nsql/nsql_exec.py
+++ server/binder/nsql/nsql_exec.py
@@ -24,7 +24,7 @@ class Executor(object):
         result = db.execute_query(sql)
         return result
 
-    def nsql_exec(self, nsql: str, db: NeuralDB, verbose=True):
+    def nsql_exec(self, nsql: str, db: NeuralDB, verbose=True, extract=False):
         steps = []
         root_node = get_cfg_tree(nsql)  # Parse execution tree from server.binder.nsql.
         get_steps(root_node, steps)  # Flatten the execution tree and get the steps.
@@ -149,4 +149,4 @@ class Executor(object):
 
             else:
                 sub_table = self.sql_exec(nsql, db, verbose=verbose)
-                return sub_table #extract_answers(sub_table)
+                return extract_answers(sub_table) if extract else sub_table
