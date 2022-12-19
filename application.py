3349c9fe81db1e6710ef0dd2b5a5a3bdb5d76188
Maksim Ivanov <m0597@berkeley.edu>
2022-12-19 18:50:36 -0500

    Format

--- application.py
+++ application.py
@@ -16,34 +16,41 @@ from processor import TableProcessor, PlotterProcessor
 # load S3 credentials from environment variables
 S3_KEY = os.environ.get("S3_KEY")
 S3_SECRET = os.environ.get("S3_SECRET")
-BUCKET_NAME = 'rango-data'
-DEFAULT_DATA_DIR = 'default'
-USER_DATA_DIR = 'user'
+BUCKET_NAME = "rango-data"
+DEFAULT_DATA_DIR = "default"
+USER_DATA_DIR = "user"
 
 application = Flask(__name__)
-CORS(application,
-     origins=['http://localhost', 'http://localhost:3000/', 'https://feature-amplify.doyldq2ymzq2e.amplifyapp.com',
-              'https://rango.run', 'https://www.app.rango.run'])
+CORS(
+    application,
+    origins=[
+        "http://localhost",
+        "http://localhost:3000/",
+        "https://feature-amplify.doyldq2ymzq2e.amplifyapp.com",
+        "https://rango.run",
+        "https://www.app.rango.run",
+    ],
+)
 
 
-@application.route('/', methods=['POST'])
+@application.route("/", methods=["POST"])
 def plot():
     dataset, query = preprocess_data_request(request)
     processor = PlotterProcessor(dataset, query)
     return processor.produce_payload()
 
 
-@application.route('/table', methods=['POST'])
+@application.route("/table", methods=["POST"])
 def table():
     dataset, query = preprocess_data_request(request)
     processor = TableProcessor(dataset, query)
     return processor.produce_payload()
 
 
-@application.route('/upload', methods=['POST'])
+@application.route("/upload", methods=["POST"])
 def upload():
     # fetch the form from the data
-    token = request.args.get('token')
+    token = request.args.get("token")
     if not token:
         token = str(uuid.uuid4())
 
@@ -51,45 +58,54 @@ def upload():
     for name, file_storage in request.files.items():
         dataset = pd.read_csv(file_storage)
         name = secure_filename(name)
-        dataset.to_csv(f's3://{BUCKET_NAME}/{USER_DATA_DIR}/{token}/{name}',
-                       storage_options={'key': S3_KEY, 'secret': S3_SECRET}, index=False)
+        dataset.to_csv(
+            f"s3://{BUCKET_NAME}/{USER_DATA_DIR}/{token}/{name}",
+            storage_options={"key": S3_KEY, "secret": S3_SECRET},
+            index=False,
+        )
     return token
 
 
 def load_from_s3(name, token):
     name = secure_filename(name)
-    s3 = boto3.resource('s3', aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
-    bucket = s3.Bucket('{BUCKET_NAME}')
-    default_files = list([obj.key for obj in bucket.objects.filter(Prefix='default')])
-    if 'default/' + name in default_files:
-        return pd.read_csv(f's3://{BUCKET_NAME}/{DEFAULT_DATA_DIR}/{name}',
-                           storage_options={'key': S3_KEY, 'secret': S3_SECRET})
+    s3 = boto3.resource("s3", aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
+    bucket = s3.Bucket("{BUCKET_NAME}")
+    default_files = list([obj.key for obj in bucket.objects.filter(Prefix="default")])
+    if "default/" + name in default_files:
+        return pd.read_csv(
+            f"s3://{BUCKET_NAME}/{DEFAULT_DATA_DIR}/{name}",
+            storage_options={"key": S3_KEY, "secret": S3_SECRET},
+        )
     if token:
-        user_files = list([obj.key for obj in bucket.objects.filter(Prefix=f'user/{token}')])
-        if f'user/{token}/' + name in user_files:
-            return pd.read_csv(f's3://{BUCKET_NAME}/{USER_DATA_DIR}/{token}/{name}',
-                               storage_options={'key': S3_KEY, 'secret': S3_SECRET})
-    raise ValueError(f'File {name} not found')
+        user_files = list(
+            [obj.key for obj in bucket.objects.filter(Prefix=f"user/{token}")]
+        )
+        if f"user/{token}/" + name in user_files:
+            return pd.read_csv(
+                f"s3://{BUCKET_NAME}/{USER_DATA_DIR}/{token}/{name}",
+                storage_options={"key": S3_KEY, "secret": S3_SECRET},
+            )
+    raise ValueError(f"File {name} not found")
 
 
 def preprocess_data_request(request):
     data = request.form
-    dataset_name = data.get('name', 'default')
-    user_token = request.args.get('token', None)
+    dataset_name = data.get("name", "default")
+    user_token = request.args.get("token", None)
     dataset = load_from_s3(dataset_name, user_token)
-    query = data['query']
+    query = data["query"]
     return dataset, query
 
 
-@application.route('/generate_token', methods=['GET'])
+@application.route("/generate_token", methods=["GET"])
 def generate_auth_token():
     data = request.args
-    if 'token' in data:
-        return data['token']  # do not create a new token
+    if "token" in data:
+        return data["token"]  # do not create a new token
     token = str(uuid.uuid4())
     return token
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     application.debug = True
     application.run()
