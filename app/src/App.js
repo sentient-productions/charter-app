05975dea9d6f8b4dc0d406ab87f657a00be7b32a
Owen Colegrove <owen@algofi.org>
2022-12-20 13:50:57 -0500

    check in changes

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
-import { Box, Card, Grid } from '@mui/material';
+import { Box, Card, Grid, Typography } from '@mui/material';
 import CodeSnippet from './CodeSnippet';
 import Data from './Data';
+import Footer from './Footer';
+import LocalAppBar from './AppBar';
 import Output from './Output';
 import QueryBar from './QueryBar';
 import { QUERY_MODES } from './constants';
 
 function App() {
-  
+
   // Global application state
   const [state, setState] = useState({
-    dataset: 'salaries.csv',
+    dataset: 'cars.csv',
     columns: [],
     rows: [],
     raw: [],
@@ -24,41 +26,52 @@ function App() {
   });
 
   return (
-    <Grid container alignItems="center" justifyContent="center">
-      <Grid item xs={8} sx={{ mt: 10 }}>
-        {/* Query Panel */}
-        <Grid item sx={{ mb: 2.5 }}>
-          <QueryBar state={state} setState={setState} />
-        </Grid>
-        {/* Code Snippet Panel */}
-        <Grid item sx={{ mb: state.isExecuted ? 2.5 : 0 }}>
-          <CodeSnippet state={state} setState={setState} />
-        </Grid>
-        {/* Output Panel */}
-        <Grid item sx={{ mb: state.isExecuted ? 2.5 : 0 }}>
-          {/* 
-                TODO - move this to it's own component
-                This was challenging b/c we need the id="graph-div" 
-                to be associated with this page to render the graph  
-          */}
-          <Card
-            sx={
-              state.isExecuted
-                ? { pl: 2, pr: 2, pt: 1, pb: 1 }
-                : { pl: 2, pr: 2 } // no height if no output, but pre-pad l+r for plot balance
-            }
-          >
-            <Output state={state} setState={setState} />
-            {/* this is the line that prevents us from putting more logic in the Output component */}
-            <Box id="graph-div" />
-          </Card>
-        </Grid>
-        {/* Dataset information */}
-        <Grid item>
-          <Data state={state} setState={setState} />
+    <>
+      <LocalAppBar />
+      <Grid container alignItems="center" justifyContent="center">
+        <Grid item sm={7} xs={12} sx={{ mt: 10 }}>
+          <Grid item container alignItems="center" justifyContent="center">
+            <Typography variant="h4" sx={{ mb: 2 }}>
+              Data Wrangler
+            </Typography>
+          </Grid>
+          {/* Query Panel */}
+          <Grid item sx={{ mb: 2. }}>
+            <QueryBar state={state} setState={setState} />
+          </Grid>
+          {/* Code Snippet Panel */}
+          <Grid item sx={{ mb: state.isExecuted ? 2. : 0 }}>
+            <CodeSnippet state={state} setState={setState} />
+          </Grid>
+          {/* Output Panel */}
+          <Grid item sx={{ mb: state.isExecuted ? 2. : 0 }}>
+            {/* 
+                  TODO - move this to it's own component
+                  This was challenging b/c we need the id="graph-div" 
+                  to be associated with this page to render the graph  
+            */}
+            <Card
+              sx={
+                state.isExecuted
+                  ? { pl: 2, pr: 2, pt: 1, pb: 1 }
+                  : { pl: 2, pr: 2 } // no height if no output, but pre-pad l+r for plot balance
+              }
+            >
+              <Output state={state} setState={setState} />
+              {/* this is the line that prevents us from putting more logic in the Output component */}
+              <Box id="graph-div" />
+            </Card>
+          </Grid>
+          {/* Dataset information */}
+          <Grid item>
+            <Data state={state} setState={setState} />
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+      <Grid >
+        <Footer />
+      </Grid>
+    </>
   );
 }
 
--- app/src/index.js
+++ app/src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Helmet } from "react-helmet";
+import { NAME } from './constants';
 
 const theme = createTheme({
   palette: {
     mode: 'dark',
     background: {
-      default: '#212121' // Set the default background color to a light gray
+      // default: '#232023' // Set the default background color to a light gray
+      // default: '#18151A', // Set the default background color to a light gray
+      paper: '#000000'
     },
     primary: {
-      main: '#ff4400' // Use a bright, bold color for the primary theme
+      main: '#C70039' // Use a bright, bold color for the primary theme
     },
     secondary: {
-      main: '#0044ff' // Use a cool, muted color for the secondary theme
+      main: '#FF5733' // Use a cool, muted color for the secondary theme
     },
     text: {
       primary: '#ffffff',
@@ -22,7 +26,7 @@ const theme = createTheme({
     }
   },
   typography: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontFamily: "Roboto",
     fontSize: 14,
     fontWeightLight: 300,
     fontWeightRegular: 400,
@@ -36,17 +40,17 @@ const theme = createTheme({
         backgroundColor: '#0044ff' // Use the secondary theme color as the background
       }
     },
-    MuiCard: {
-      root: {
-        borderRadius: 12, // Round the edges of the card
-        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' // Add a subtle shadow
-      }
-    }
   }
 });
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <title>{NAME} | AI Powered Data Wrangling. </title>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
+    </Helmet>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <App />
