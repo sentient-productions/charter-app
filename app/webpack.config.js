78cb61506934adfda19fa5e6fd7c48b23c92cb5f
Maksim Ivanov <m0597@berkeley.edu>
2022-12-20 09:56:07 -0500

    Re-add missing app files

---
+++ app/webpack.config.js
@@ -0,0 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); //to access built-in plugins
+
+module.exports = {
+  module: {
+    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+};
