8d6ff8160784abd1487c03d038fd8309a8570070
Maksim Ivanov <m0597@berkeley.edu>
2022-12-21 10:08:40 -0500

    clean up and add file formats

--- server/application.py
+++ server/application.py
@@ -3,25 +3,15 @@ import os
 import sys
 import uuid
 
-import boto3
-import pandas as pd
 from flask import Flask
 from flask import request
 from flask_cors import CORS
 
-# local
-from werkzeug.utils import secure_filename
+
 ROOT_DIR = os.path.join(os.path.dirname(__file__), "..")
 sys.path.insert(0, ROOT_DIR)
 from processor import TableProcessor, PlotterProcessor
-
-# load S3 credentials from environment variables
-S3_KEY = os.environ.get("S3_KEY")
-S3_SECRET = os.environ.get("S3_SECRET")
-BUCKET_NAME = "rango-data"
-DEFAULT_DATA_DIR = "default"
-USER_DATA_DIR = "user"
-STORAGE_OPTIONS = {"key": S3_KEY, "secret": S3_SECRET} if S3_KEY and S3_SECRET else None
+from utils import *
 
 application = Flask(__name__)
 CORS(
@@ -64,8 +54,13 @@ def upload():
     # Each key in :attr:`files` is the name from the ``<input type="file" name="">``
     for file_storage in request.files.values():
         name = file_storage.filename
-        dataset = pd.read_csv(file_storage)
+        # check if file is an excel file
+
+        dataset = pd_read_with_format(file_storage)
         name = secure_filename(name)
+        # replace name file extension with csv
+        name = name.rsplit(".", 1)[0] + ".csv"
+
         dataset.to_csv(
             f"s3://{BUCKET_NAME}/{USER_DATA_DIR}/{token}/{name}",
             storage_options=STORAGE_OPTIONS,
@@ -73,28 +68,29 @@ def upload():
         )
     return token
 
+
 @application.route("/list-user-files", methods=["GET"])
 def list_user_files():
     token = request.args.get("token")
     if not token:
-        return ''
+        return ""
     s3 = boto3.resource("s3", aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
     bucket = s3.Bucket(BUCKET_NAME)
     user_files = list(
         [obj.key for obj in bucket.objects.filter(Prefix=f"user/{token}")]
     )
 
-    return ','.join([file.split("/")[-1] for file in user_files])
+    return ",".join([file.split("/")[-1] for file in user_files])
+
 
 @application.route("/list-default-files", methods=["GET"])
 def list_default_files():
     s3 = boto3.resource("s3", aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
     bucket = s3.Bucket(BUCKET_NAME)
-    user_files = list(
-        [obj.key for obj in bucket.objects.filter(Prefix=f"default/")]
-    )
+    user_files = list([obj.key for obj in bucket.objects.filter(Prefix=f"default/")])
 
-    return ','.join([file.split("/")[-1] for file in user_files])
+    return ",".join([file.split("/")[-1] for file in user_files])
+
 
 @application.route("/generate-token", methods=["GET"])
 def generate_auth_token():
@@ -104,6 +100,7 @@ def generate_auth_token():
     token = str(uuid.uuid4())
     return token
 
+
 @application.route("/get-dataset", methods=["POST"])
 def get_dataset_as_csv():
     name = request.form.get("name", "salaries.csv")
@@ -111,36 +108,6 @@ def get_dataset_as_csv():
     token = request.form.get("token")
     dataset = load_from_s3(name, token)
     return dataset.to_csv(index=False)
-
-def load_from_s3(name, token):
-    name = secure_filename(name)
-    s3 = boto3.resource("s3", aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
-    bucket = s3.Bucket(BUCKET_NAME)
-    default_files = list([obj.key for obj in bucket.objects.filter(Prefix="default")])
-    if f"{DEFAULT_DATA_DIR}/" + name in default_files:
-        return pd.read_csv(
-            f"s3://{BUCKET_NAME}/{DEFAULT_DATA_DIR}/{name}",
-            storage_options=STORAGE_OPTIONS,
-        )
-    if token:
-        user_files = list(
-            [obj.key for obj in bucket.objects.filter(Prefix=f"user/{token}")]
-        )
-        if f"{USER_DATA_DIR}/{token}/" + name in user_files:
-            return pd.read_csv(
-                f"s3://{BUCKET_NAME}/{USER_DATA_DIR}/{token}/{name}",
-                storage_options=STORAGE_OPTIONS,
-            )
-    raise ValueError(f"File {name} not found")
-
-
-def load_dataset_and_query(request):
-    data = request.form
-    dataset_name = data.get("name", "default")
-    user_token = data.get("token", None)
-    dataset = load_from_s3(dataset_name, user_token)
-    query = data["query"]
-    return dataset, query
 
 if __name__ == "__main__":
     application.debug = True
