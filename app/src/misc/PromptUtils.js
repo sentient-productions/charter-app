0de7768c3a378c0cb0df64f99173da3e2605c36c
Owen Colegrove <owen@algofi.org>
2023-03-09 11:15:48 -0500

    reduce available prompts

--- app/src/misc/PromptUtils.js
+++ app/src/misc/PromptUtils.js
@@ -50,102 +50,102 @@ export const chatPayloads = {
     longName: "Dual Mode Doc [recommended]",
     assistant_prompt: promptJSON["DOC-w-Dual"].assistant,
   },
-  "Developer-Mode": {
-    data: [
-      {
-        role: "system",
-        content: promptJSON["Developer-Mode"].system,
-        preFilled: true,
-        messageId: 0,
-      },
-      {
-        role: "user",
-        content: "Hello, we are going to play a game.",
-        preFilled: true,
-        messageId: 1,
-      },
-      {
-        role: "assistant",
-        content: "I understand.",
-        preFilled: true,
-        messageId: 2,
-      },
-    ],
-    longName: "Developer Mode",
-    assistant_prompt: promptJSON["Developer-Mode"].assistant,
-  },
-  "Better-Dan": {
-    data: [
-      {
-        role: "system",
-        content: promptJSON["Better-Dan"].system,
-        preFilled: true,
-        messageId: 0,
-      },
-      {
-        role: "user",
-        content: "Hello, we are going to play a game.",
-        preFilled: true,
-        messageId: 1,
-      },
-      {
-        role: "assistant",
-        content: "I understand.",
-        preFilled: true,
-        messageId: 2,
-      },
-    ],
-    longName: "BetterDAN",
-    assistant_prompt: promptJSON["Better-Dan"].assistant,
-  },
-  "Jail-Break": {
-    data: [
-      {
-        role: "system",
-        content: promptJSON["Jail-Break"].system,
-        preFilled: true,
-        messageId: 0,
-      },
-      {
-        role: "user",
-        content: "Hello, we are going to play a game.",
-        preFilled: true,
-        messageId: 1,
-      },
-      {
-        role: "assistant",
-        content: "I understand.",
-        preFilled: true,
-        messageId: 2,
-      },
-    ],
-    longName: "JailBreak",
-    assistant_prompt: promptJSON["Jail-Break"].assistant,
-  },
-  "Schizo-Mode": {
-    data: [
-      {
-        role: "system",
-        content: promptJSON["Schizo-Mode"].system,
-        preFilled: true,
-        messageId: 0,
-      },
-      {
-        role: "user",
-        content: "Enable Schizo Mode",
-        preFilled: true,
-        messageId: 1,
-      },
-      {
-        role: "assistant",
-        content: "Schizo Mode enabled",
-        preFilled: true,
-        messageId: 2,
-      },
-    ],
-    longName: "Schizo Mode",
-    assistant_prompt: promptJSON["Schizo-Mode"].assistant,
-  },
+  // "Developer-Mode": {
+  //   data: [
+  //     {
+  //       role: "system",
+  //       content: promptJSON["Developer-Mode"].system,
+  //       preFilled: true,
+  //       messageId: 0,
+  //     },
+  //     {
+  //       role: "user",
+  //       content: "Hello, we are going to play a game.",
+  //       preFilled: true,
+  //       messageId: 1,
+  //     },
+  //     {
+  //       role: "assistant",
+  //       content: "I understand.",
+  //       preFilled: true,
+  //       messageId: 2,
+  //     },
+  //   ],
+  //   longName: "Developer Mode",
+  //   assistant_prompt: promptJSON["Developer-Mode"].assistant,
+  // },
+  // "Better-Dan": {
+  //   data: [
+  //     {
+  //       role: "system",
+  //       content: promptJSON["Better-Dan"].system,
+  //       preFilled: true,
+  //       messageId: 0,
+  //     },
+  //     {
+  //       role: "user",
+  //       content: "Hello, we are going to play a game.",
+  //       preFilled: true,
+  //       messageId: 1,
+  //     },
+  //     {
+  //       role: "assistant",
+  //       content: "I understand.",
+  //       preFilled: true,
+  //       messageId: 2,
+  //     },
+  //   ],
+  //   longName: "BetterDAN",
+  //   assistant_prompt: promptJSON["Better-Dan"].assistant,
+  // },
+  // "Jail-Break": {
+  //   data: [
+  //     {
+  //       role: "system",
+  //       content: promptJSON["Jail-Break"].system,
+  //       preFilled: true,
+  //       messageId: 0,
+  //     },
+  //     {
+  //       role: "user",
+  //       content: "Hello, we are going to play a game.",
+  //       preFilled: true,
+  //       messageId: 1,
+  //     },
+  //     {
+  //       role: "assistant",
+  //       content: "I understand.",
+  //       preFilled: true,
+  //       messageId: 2,
+  //     },
+  //   ],
+  //   longName: "JailBreak",
+  //   assistant_prompt: promptJSON["Jail-Break"].assistant,
+  // },
+  // "Schizo-Mode": {
+  //   data: [
+  //     {
+  //       role: "system",
+  //       content: promptJSON["Schizo-Mode"].system,
+  //       preFilled: true,
+  //       messageId: 0,
+  //     },
+  //     {
+  //       role: "user",
+  //       content: "Enable Schizo Mode",
+  //       preFilled: true,
+  //       messageId: 1,
+  //     },
+  //     {
+  //       role: "assistant",
+  //       content: "Schizo Mode enabled",
+  //       preFilled: true,
+  //       messageId: 2,
+  //     },
+  //   ],
+  //   longName: "Schizo Mode",
+  //   assistant_prompt: promptJSON["Schizo-Mode"].assistant,
+  // },
   Custom: {
     data: [
       {
